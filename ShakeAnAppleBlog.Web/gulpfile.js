384a2116ea876e7fcdfcd73a3bf3c9bfa4252159
Polina <xweird.sun@gmail.com>
2016-08-26 16:57:49 +0300

    playing with aspnet core

---
+++ ShakeAnAppleBlog.Web/gulpfile.js
@@ -0,0 +1,29 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('restore', function() {
+    gulp.src([
+        'node_modules/@angular/**/*.js',
+        'node_modules/angular2-in-memory-web-api/*.js',
+        'node_modules/rxjs/**/*.js',
+        'node_modules/systemjs/dist/*.js',
+        'node_modules/zone.js/dist/*.js',
+        'node_modules/core-js/client/*.js',
+        'node_modules/reflect-metadata/reflect.js',
+        'node_modules/jquery/dist/*.js',
+        'node_modules/bootstrap/dist/**/*.*'
+    ]).pipe(gulp.dest('./wwwroot/libs'));
+});
+
+gulp.task('', function () {
+    gulp.
+})
